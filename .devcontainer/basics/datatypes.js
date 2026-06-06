eef7991add81984fa8ef024a9e8f378c97af973a
Neha Megharaja Gujaran <neha.mgujaran@gmail.com>
2026-06-06 06:54:05 +0000

    datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//datatypes in javascript and treat all js code as newer version
+
+//alert(3+3) //we are using nodejs not browser
+
+let name="Neha"
+let age=19
+let isStudent=true
+let state=null;
+
+
+//number=>2 to power 53
+//bigint=>greater than 2 to power 53
+//string=>sequence of characters and written in""
+//boolean=>true or false
+//null=>intentional absence of value
+//undefined=>variable declared but not assigned a value
+//symbol=>unique identifier
+
+console.log(typeof name)
+console.log(typeof "Neha")
+console.log(typeof undefined);
+console.log(typeof null);//object
+
