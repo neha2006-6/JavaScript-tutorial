f4b55fc1735f1efe0b22dc8e0bfa3c328c38cef9
Neha Megharaja Gujaran <neha.mgujaran@gmail.com>
2026-06-06 08:35:08 +0000

    conversion

--- .devcontainer/basics/conversionOperation.js
+++ .devcontainer/basics/conversionOperation.js
@@ -13,7 +13,7 @@ let valueInNumber=Number(score)
 
 let isLoggedIn="neha";
 let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //if we give 1 as a value in isloggedin it will return true
 //0->then false
@@ -24,4 +24,40 @@ console.log(booleanIsLoggedIn);
 
 let someNumber=33;
 let stringNumber=String(someNumber);
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+//****************************************** */
+
+let value=3;
+let negvalue=-value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2); //modulus operator gives the remainder    
+
+let str1='hello';
+let str2='world';
+// console.log(str1+str2); //concatenation of string
+
+let str3='hello';
+let str4='world';
+// console.log(str3*str4); //NaN not a number
+console.log('1'+'2');
+
+
+console.log(-true);
+console.log(+true);
+console.log(-false);
+console.log(+false);
+console.log(-'');
+console.log(+'');
+
+let gamecounter=100;
+gamecounter++;
+console.log(gamecounter);
+gamecounter--;
+console.log(gamecounter);
+
