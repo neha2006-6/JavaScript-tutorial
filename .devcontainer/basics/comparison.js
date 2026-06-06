302cdd97bf52456aa5f506d68bcc7d3f08159272
Neha Megharaja Gujaran <neha.mgujaran@gmail.com>
2026-06-06 08:44:11 +0000

    comparison

---
+++ .devcontainer/basics/comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1)
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+ 
+console.log("02">1)
+console.log("2"<1);
+
+console.log(null>1);    
+console.log(null==0);
+//avoid null and underfined
+console.log(undefined==null);
+console.log(undefined===0);
+
