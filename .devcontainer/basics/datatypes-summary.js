5e6baaf0a0286ea333f020e29dea2c7ea4e5c603
Neha Megharaja Gujaran <neha.mgujaran@gmail.com>
2026-06-06 09:04:19 +0000

    datatype summary

---
+++ .devcontainer/basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+//primitive datatypes
+
+//7 types
+//1.string
+//2.number
+//3.boolean
+//4.null
+//5.undefined
+//6.symbol
+//7.bigint
+
+// const score=100;
+// const score2=100.3
+
+const is=Symbol('123')
+const is1=Symbol('123')
+
+console.log(is==is1);
+
+const big=135612555555821n;
+console.log(big);
+
+//NON primitive datatypes
+//array,objects,function
+
+const hero=["ironman","spiderman","thor"]; 
+let myobject={
+    name:"neha",
+    age:22
+
+}
+
+ const myfunction=function(){
+    console.log("hello world");
+ }
+
+ 
