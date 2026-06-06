2b548194c2e7301bc6358302e696ba0cfac314e4
Neha Megharaja Gujaran <neha.mgujaran@gmail.com>
2026-06-06 07:09:36 +0000

    conversion

---
+++ .devcontainer/basics/conversionOperation.js
@@ -0,0 +1,27 @@
+let score="33abs"; 
+let num1=null;
+let num2=undefined;
+let num3=true;
+
+// console.log(typeof score);
+// console.log(typeof num1);
+// console.log(typeof num2);
+// console.log(typeof num3);
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+
+let isLoggedIn="neha";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//if we give 1 as a value in isloggedin it will return true
+//0->then false
+//""->false
+//"neha"->true
+//null->false
+//undefined->false
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+console.log(typeof stringNumber);
