06f5522c1bf422179f1d27304a18778e9fb71b50
Neha Megharaja Gujaran <neha.mgujaran@gmail.com>
2026-06-07 03:16:42 +0000

    string

---
+++ .devcontainer/basics/string.js
@@ -0,0 +1,30 @@
+const name="neha"   
+const reop=50
+
+// console.log(name+reop+"value");//it is not used
+
+// console.log(`Hello my name is ${name} and my repo name is ${reop}`)
+
+const gamename = new String("Neha")
+// console.log(gamename)
+// console.log(gamename[0]);
+// console.log(gamename._proto__);
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.toLowerCase());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf("h"));
+// console.log(gamename.lastIndexOf("a"));
+// console.log(gamename.slice(-3,3));
+
+
+const newstring="   Hello World   "
+console.log(newstring);
+console.log(newstring.trim());
+
+const url="https://www.youtube.com/watch?v=H9t2sQZyqj8"
+console.log(url.replace('v=H9t','v20'));
+console.log(url.includes('neha'));
+
+console.log(gamename.split("e"));
