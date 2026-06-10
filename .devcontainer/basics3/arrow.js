c93d2068c57f6bf3ee021e842e07b484b172139d
Neha Megharaja Gujaran <neha.mgujaran@gmail.com>
2026-06-10 04:05:36 +0000

    arrow

---
+++ .devcontainer/basics3/arrow.js
@@ -0,0 +1,29 @@
+const user =
+{
+    username : "neha",
+    price :898,
+    welcomemessage : function()
+    {
+console.log(`${this.username},welcomeee`);
+// console.log(this)
+    }
+}
+user.welcomemessage()
+console.log(this)
+
+ const chai = () =>
+{
+    let uname = "neha"
+    console.log(this)
+
+}
+// chai()
+const two=(n1,n2) => (n1+n2)
+
+
+console.log(two(3,4))
+
+const myarray = [2,4,5,6]
+
+
+
