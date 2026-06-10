cf88feb227e59b58bd87acb7ed0d1a4b26b5c3a6
Neha Megharaja Gujaran <neha.mgujaran@gmail.com>
2026-06-10 04:12:58 +0000

    iife

---
+++ .devcontainer/basics3/iife.js
@@ -0,0 +1,17 @@
+// function chai()
+// {
+//     console.log("neha")
+// }
+// chai()
+
+(function chai()
+{
+    console.log("neha")
+})
+();//name iife
+
+( (name) => {
+    console.log(`Db connected ${name}`)
+})("nisha")//unamed iife
+
+
