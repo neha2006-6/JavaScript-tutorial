4e718038f95b2241e03f7de3fbfb1f446c134791
Neha Megharaja Gujaran <neha.mgujaran@gmail.com>
2026-06-11 09:33:03 +0000

    loops

---
+++ .devcontainer/loops/three.js
@@ -0,0 +1,18 @@
+const coding=["js","ruby","java"]
+coding.forEach(function (val){
+
+    console.log(val)
+})
+
+coding.forEach((item)=>{
+    console.log(item);
+})
+function printw(item)
+{
+    console.log(item);
+}
+coding.forEach(printme)
+
+// coding.forEach((item,indexedDB,arr)=>{
+// console.log(item,indexedDB,arr)
+// })
