e502084af7728c7025bf1a09149d7aeca67b3bec
Neha Megharaja Gujaran <neha.mgujaran@gmail.com>
2026-06-11 05:31:57 +0000

    switch

---
+++ .devcontainer/controlflow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march";
+switch (month) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "march":
+        console.log("feb");
+        break;
+    case "april":
+        console.log("march");
+        break;
+    case "hhhjh":
+        console.log("april");
+        break;
+
+    default:console.log("belom");
+        break;
+}
