de6b8242cc7ce86315d28df9b9f85809d062eefb
Neha Megharaja Gujaran <neha.mgujaran@gmail.com>
2026-06-11 09:33:44 +0000

    object

---
+++ .devcontainer/loops/ob.js
@@ -0,0 +1,14 @@
+const myobj={
+    js:'javascript',
+    cpp:'cpp',
+
+}
+
+for (const key in myobj) {
+    console.log(`${key} for ${myobj[key]}`);
+}
+const program=["js","rt","rg"]
+for(const key in program)
+{
+    console.log(program[key]);
+}
