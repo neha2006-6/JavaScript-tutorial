bac5bb0c40565e26d56531825f21df5fe74de872
Neha Megharaja Gujaran <neha.mgujaran@gmail.com>
2026-06-11 05:26:30 +0000

    condition

---
+++ .devcontainer/controlflow/one.js
@@ -0,0 +1,46 @@
+//if
+
+
+const isUser=true;
+// if()
+// {
+// console.log("ecuted");
+// }
+// else{
+
+// }
+// <,.,<=,>=,==,>=,=assignmet ,==check ,!=,===
+
+// const sc=200;
+// if(sc>100)
+// {
+//     const power="neha"
+//     console.log(`my name is ${power}`)
+// }
+
+
+// const bal=1000
+// if(bal>500) console.log("test"),
+// console.log("test2")
+
+// if(bal<500)
+// {
+// console.log("less than ")
+// }
+// else if(bal<750)
+// {
+//     console.log("pate")
+// }
+// else{
+//     console.log("heuu")
+// }
+
+const user= true;
+const card = true;
+const google=false
+if(user && google)
+{
+    console.log("true");
+}
+
+
